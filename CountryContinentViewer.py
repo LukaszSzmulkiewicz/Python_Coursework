91474e1808dee65bbc8dbab3ebc21c1d19797dd0
Lukasz Szmulkiewicz <55105517+LukaszSzmulkiewicz@users.noreply.github.com>
2022-11-15 10:27:49 +0000

    solution code uses a class CCV

--- CountryContinentViewer.py
+++ CountryContinentViewer.py
@@ -1,11 +1,6 @@
 #!/usr/bin/env python
 #!/bin/env python
-# Simple regular expression examples
 
-import sys
-import fileinput
-import re
-import string
 import matplotlib.pyplot as plt
 import pandas as pd
 import json
@@ -14,7 +9,7 @@ class CountryContinentViewer:
 
   df_user_data = pd.DataFrame(columns=[ 'country_code', 'country_name', 'continent'])
 
-  def load_json():
+  def load_json(self):
 
     # file_name = input("Enter File Name:")
     # print("File name is: " + file_name)
@@ -26,23 +21,23 @@ class CountryContinentViewer:
                 issuu_user_data.append(user_data) 
     return issuu_user_data
 
-  def load_country_codes(issuu_user_data):
+  def load_country_codes(self, issuu_user_data):
     country_codes = []
     for country in issuu_user_data:
         country_codes.append(country["visitor_country"])
     return country_codes
     
-  def load_countries_to_dictionary():
+  def load_countries_to_dictionary(self):
     df_country_and_code = pd.read_csv("data/countries.csv")
     dict_country_and_code = dict(zip(df_country_and_code.country_code , df_country_and_code.name))
     return dict_country_and_code
   
-  def load_continents_to_dictionary():
+  def load_continents_to_dictionary(self):
     df_country_and_continent = pd.read_csv("data/continents.csv")
     dict_country_and_continent = dict(zip(df_country_and_continent.country , df_country_and_continent.continent))
     return dict_country_and_continent
 
-  def assign_countries_and_continents(country_codes, dict_country_and_code, dict_country_and_continent):
+  def assign_countries_and_continents(self, country_codes, dict_country_and_code, dict_country_and_continent):
     for country in country_codes:
           if dict_country_and_code[country]:
               if dict_country_and_continent[dict_country_and_code[country]]:
@@ -53,16 +48,12 @@ class CountryContinentViewer:
                       'continent': dict_country_and_continent[dict_country_and_code[country]]
                   }
                   df_new_row = pd.DataFrame([new_row])
-                  df_user_data = pd.concat([df_user_data, df_new_row], axis=0, ignore_index=True)
+                  self.df_user_data = pd.concat([self.df_user_data, df_new_row], axis=0, ignore_index=True)
           else:
               continue
-    return df_user_data
+    return self.df_user_data
           
 
-  # print(df_user_data)
-
-  # for key, value in dict_country_and_continent.items() :
-  #     print (key)
   def plot_country(self):
     df_countries_count = self.df_user_data['country_name'].value_counts().rename_axis('country_name').reset_index(name='counts')
 
