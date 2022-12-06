a2a2cf59efa787477fbd13711a782292301dc75b
Lukasz Szmulkiewicz <55105517+LukaszSzmulkiewicz@users.noreply.github.com>
2022-12-06 05:46:40 +0000

    created task 7

--- solution.py
+++ solution.py
@@ -7,6 +7,7 @@ import CountryContinentViewer
 import HelperMethods
 import ReaderCounter
 import BrowserViewer
+import tkinter as tk
 
 ccv = CountryContinentViewer.CountryContinentViewer()
 hm = HelperMethods.HelperMethods()
@@ -49,17 +50,25 @@ def task_2_setup(file_name, taks_number):
 def task_3_setup(file_name, taks_number):
     # loading the issuu user data 
     issuu_user_data = hm.load_json_specific(file_name, 'visitor_useragent')
-
-    #getting user agent code from the json objects
-    list_browser_codes = bv.parse_user_agents(issuu_user_data)
-
-    #converting list of browser codes to dataframe
-    df_browser_codes = bv.browsers_to_dataframe(list_browser_codes)
-
-    #count browser code occurences
-    df_browser_codes_count = bv.count_browser_codes_occurrences(df_browser_codes)
-
+   
     if taks_number == "3a":
+      #converting list of browser codes to dataframe
+      df_browser_codes = bv.browsers_to_dataframe(issuu_user_data)
+
+      #count browser code occurences
+      df_browser_codes_count = bv.count_browser_codes_occurrences(df_browser_codes)
+
+      return df_browser_codes_count
+    elif taks_number == "3b":
+      #getting user agent code from the json objects
+      list_browser_codes = bv.parse_user_agents(issuu_user_data)
+
+      #converting list of browser codes to dataframe
+      df_browser_codes = bv.browsers_to_dataframe(list_browser_codes)
+
+      #count browser code occurences
+      df_browser_codes_count = bv.count_browser_codes_occurrences(df_browser_codes)
+
       return df_browser_codes_count
 
 # method to setup data for plotting of task 4
@@ -79,15 +88,71 @@ def task_4_setup(file_name):
   df_result = rc.return_10_best_readers(df_readers_count)
   return df_result
 
+def task_7_setup():
+  
+
+  def get_task2a_command():
+    df_user_data = task_2_setup(file_name.get(), '2a')
+    return ccv.plot_country(df_user_data)
+  
+  def get_task2b_command():
+    df_user_data = task_2_setup(file_name.get(), '2b')
+    return ccv.plot_continent(df_user_data)
+    
+  def get_task3a_command():
+    df_user_data = task_3_setup(file_name.get(), '3a')
+    return bv.plot_browsers(df_user_data)
+  
+  def get_task3b_command():
+    df_user_data = task_3_setup(file_name.get(), '3b')
+    return bv.plot_browsers(df_user_data)
+  
+  def get_task4_command():
+    df_user_data = task_4_setup(file_name.get())
+    return rc.plot_readers(df_user_data)
+  
+  def get_task5_command():
+    print("task 5 to be done")
+  
+  def get_task6_command():
+    print("task 6 to be done")
+  
+  
+  root = tk.Tk()
+  root.geometry("300x500")
+  root.title("Coursework_issuu_10")
+
+  label = tk.Label(root, text="File Name", font=("Arial",16))
+  label.pack()
+
+  file_name = tk.Entry(root)
+  file_name.pack()
+
+  button_Task2a = tk.Button(root, text="Task 2a", font=("Arial",16), command=lambda: get_task2a_command())
+  button_Task2a.pack(padx=10, pady=10)
+  button_Task2b = tk.Button(root, text="Task 2b", font=("Arial",16), command=lambda: get_task2b_command())
+  button_Task2b.pack(padx=10, pady=10)
+  button_Task3a = tk.Button(root, text="Task 3a", font=("Arial",16), command=lambda: get_task3a_command())
+  button_Task3a.pack(padx=10, pady=10)
+  button_Task3b = tk.Button(root, text="Task 3b", font=("Arial",16), command=lambda: get_task3b_command())
+  button_Task3b.pack(padx=10, pady=10)
+  button_Task4 = tk.Button(root, text="Task 4", font=("Arial",16), command=lambda: get_task4_command())
+  button_Task4.pack(padx=10, pady=10)
+  button_Task5 = tk.Button(root, text="Task 5d", font=("Arial",16), command=lambda: get_task5_command())
+  button_Task5.pack(padx=10, pady=10)
+  button_Task6 = tk.Button(root, text="Task 6", font=("Arial",16), command=lambda: get_task6_command())
+  button_Task6.pack(padx=10, pady=10)
+
+    
+
+  root.mainloop()
+
 
 def run(args):
   user_uuid = args.user_uuid # user UUID
   doc_uuid = args.doc_uuid # document UUID
   task_id = args.task_id # task id
   file_name = args.file_name # file name
-  
- 
-  # getting a dataframe from a class
       
 
   if task_id == "2a":
@@ -100,7 +165,8 @@ def run(args):
     df_user_data = task_3_setup(file_name, task_id)
     return bv.plot_browsers(df_user_data)
   elif task_id == "3b":
-    return print("task 3b is running")
+    df_user_data = task_3_setup(file_name, task_id)
+    return bv.plot_browsers(df_user_data)
   elif task_id == "4":
     df_user_data = task_4_setup(file_name)
     return rc.plot_readers(df_user_data)
@@ -109,7 +175,7 @@ def run(args):
   elif task_id == "6":
     return "task 6 is running"
   elif task_id == "7":
-    return "task 7 is running"
+    return task_7_setup()
 
 def main():
   parser=argparse.ArgumentParser(description="Run a coursework with a command line interface")
